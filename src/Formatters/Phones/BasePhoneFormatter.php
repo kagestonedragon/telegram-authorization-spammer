7a264e62d13fcedb4d32969fe72bb37ee3822451
Kage Stonedragon <kagestonedragon@gmail.com>
2021-07-29 01:11:34 +0300

    Delete abstract phone formatter

--- src/Formatters/Phones/BasePhoneFormatter.php
+++ src/Formatters/Phones/BasePhoneFormatter.php
@@ -2,11 +2,13 @@
 
 namespace Kagestonedragon\TelegramAuthorizationSpammer\Formatters\Phones;
 
+use Kagestonedragon\TelegramAuthorizationSpammer\Formatters\Exceptions\FormatterException;
+
 /**
- * Class PhoneFormatter
+ * Class AbstractPhoneFormatter
  * @package Kagestonedragon\TelegramAuthorizationSpammer\Formatters\Phones
  */
-class BasePhoneFormatter extends AbstractPhoneFormatter
+class BasePhoneFormatter implements PhoneFormatterInterface
 {
     /**
      * @param mixed $value
@@ -14,13 +16,62 @@ class BasePhoneFormatter extends AbstractPhoneFormatter
      */
     public function format(mixed $value): string
     {
-        return parent::format($value);
+        if (empty($value) || is_string($value) === false) {
+            throw new FormatterException(sprintf(
+                "Given value isn't correct: %s",
+                $value
+            ));
+        }
+
+        $normalizedValue = $this->normalize($value);
+
+        if ($this->validateAfterNormalization($normalizedValue) === false) {
+            throw new FormatterException(sprintf(
+                "Given value isn't correct after normalization: given %s: normalized %s",
+                $value,
+                $normalizedValue
+            ));
+        }
+
+        return $normalizedValue;
+    }
+
+    /**
+     * @param string $value
+     * @return string
+     */
+    protected function normalize(string $value): string
+    {
+        $result = [];
+
+        foreach (str_split($value) as $character) {
+            if (is_numeric($character) === false) {
+                continue;
+            }
+
+            $result[] = $character;
+        }
+
+        return implode('', $result);
+    }
+
+    /**
+     * @param string $value
+     * @return bool
+     */
+    protected function validateAfterNormalization(string $value): bool
+    {
+        if (strlen($value) < $this->getMinimalCharacters()) {
+            return false;
+        }
+
+        return true;
     }
 
     /**
      * @return int
      */
-    public function getMinimalCharacters(): int
+    protected function getMinimalCharacters(): int
     {
         return 0;
     }
@@ -33,3 +84,4 @@ class BasePhoneFormatter extends AbstractPhoneFormatter
         return 'default';
     }
 }
+
--- src/Formatters/Phones/RussianPhoneFormatter.php
+++ src/Formatters/Phones/RussianPhoneFormatter.php
@@ -6,8 +6,10 @@ namespace Kagestonedragon\TelegramAuthorizationSpammer\Formatters\Phones;
  * Class RussianPhoneFormatter
  * @package Kagestonedragon\TelegramAuthorizationSpammer\Formatters\Phones
  */
-class RussianPhoneFormatter extends AbstractPhoneFormatter
+class RussianPhoneFormatter extends BasePhoneFormatter
 {
+    protected const PHONE_PATTERN = "7%s";
+
     /**
      * +7 (999) 999 99-99 | 8 (999) 999 99-99 -> 79999999999
      *
@@ -16,19 +18,17 @@ class RussianPhoneFormatter extends AbstractPhoneFormatter
      */
     public function format(mixed $value): string
     {
-        $value = parent::format($value);
-
-        return implode('', [
-            $this->getStartsWith(),
-            $this->getUsefullPiece($value)
-        ]);
+        return sprintf(
+            static::PHONE_PATTERN,
+            $this->getUsefulPiece(parent::format($value))
+        );
     }
 
     /**
      * @param string $value
      * @return string
      */
-    protected function getUsefullPiece(string $value): string
+    protected function getUsefulPiece(string $value): string
     {
         return substr(
             $value,
@@ -43,14 +43,6 @@ class RussianPhoneFormatter extends AbstractPhoneFormatter
     protected function getMinimalCharacters(): int
     {
         return 10;
-    }
-
-    /**
-     * @return string
-     */
-    protected function getStartsWith(): string
-    {
-        return '7';
     }
 
     /**
